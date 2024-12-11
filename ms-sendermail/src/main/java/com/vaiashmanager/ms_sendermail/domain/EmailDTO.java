d6401835499aa74b4a820b05f0781bf0fde514ed
EmmanuelEcheconea <eecheconea@gmail.com>
2024-12-11 17:26:06 -0300

    MS sender mail nuevo

---
+++ ms-sendermail/src/main/java/com/vaiashmanager/ms_sendermail/domain/EmailDTO.java
@@ -0,0 +1,17 @@
+package com.vaiashmanager.ms_sendermail.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EmailDTO {
+
+    private String[] toUser;
+    private String subject;
+    private String message;
+}
---
+++ ms-sendermail/src/test/java/com/vaiashmanager/ms_sendermail/MsSendermailApplicationTests.java
@@ -0,0 +1,11 @@
+package com.vaiashmanager.ms_sendermail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSendermailApplicationTests {
+
+
+
+}
